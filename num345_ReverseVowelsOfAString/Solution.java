22d45b6cf609d073f10b00107215ad7a35aa0777
bryanbo-cao <bryan.bo.cao@outlook.com>
2016-06-20 16:59:49 -0600

    Solve num345_ReverseVowelsOfAString

---
+++ num345_ReverseVowelsOfAString/Solution.java
@@ -0,0 +1,36 @@
+/**
+https://leetcode.com/problems/reverse-vowels-of-a-string/
+345. Reverse Vowels of a String My Submissions QuestionEditorial Solution
+Total Accepted: 23406 Total Submissions: 65706 Difficulty: Easy
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Given s = "hello", return "holle".
+
+Example 2:
+Given s = "leetcode", return "leotcede".
+ */
+package algorithms.num345_ReverseVowelsOfAString;
+
+import java.util.*;
+
+public class Solution {
+    public String reverseVowels(String s) {
+        Set<Character> vset = new HashSet<Character>();
+        char[] cs = s.toCharArray();
+        vset.add('a'); vset.add('e'); vset.add('i'); vset.add('o'); vset.add('u');
+        vset.add('A'); vset.add('E'); vset.add('I'); vset.add('O'); vset.add('U');
+        List<Character> vls = new ArrayList<Character>();
+        for (int i = 0; i < cs.length; i++) if (vset.contains(cs[i])) vls.add(cs[i]);
+        Collections.reverse(vls);
+        
+        StringBuffer sb = new StringBuffer();
+        Iterator<Character> it = vls.iterator();
+        for (int i = 0; i < cs.length; i++) {
+        	if (vset.contains(cs[i])) sb.append(it.next());
+        	else sb.append(cs[i]);
+        }
+        return sb.toString();
+    }
+}
+//20160620Mon16:56 duration:8m27s15 @github.com/BryanBo-Cao,hackerrank.com/BryanBoCao,linkedin.com/in/bryanbocao
