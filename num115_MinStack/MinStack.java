b91bb331e330b71da986ff1c36df688d22f040ff
bryanbo-cao <bryan.bo.cao@outlook.com>
2016-07-05 11:25:24 -0600

    Solved num115_MinStack

---
+++ num115_MinStack/MinStack.java
@@ -0,0 +1,97 @@
+/**
+https://leetcode.com/problems/min-stack/
+155. Min Stack  QuestionEditorial Solution  My Submissions
+Total Accepted: 79172
+Total Submissions: 340812
+Difficulty: Easy
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+ */
+package algorithms.num115_MinStack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class MinStack {
+
+    /** initialize your data structure here. */
+	List<Integer> ls = null;
+	int min;
+	int minI;
+	
+    public MinStack() {
+    	ls = new ArrayList<Integer>();
+        min = 0;
+        minI = 0;
+    }
+    
+    public void push(int x) {
+    	if (ls.size() == 0) {
+    		min = x;
+    		minI = 1;
+    	}
+    	if (x < min) {
+    		min = x;
+    		minI = 1;
+    	} else minI++;	
+    	ls.add(x);
+    }
+    
+    public void pop() {
+    	int lastValue = ls.get(ls.size() - 1);
+    	ls.remove(ls.size() - 1);
+    	if (lastValue == min) {
+    		if (ls.size() == 0) {
+    			min = 0;
+    			minI = 0;
+    		} else {
+        		List<Integer> tstk = new ArrayList<Integer>();
+            	tstk.addAll(ls);
+            	Collections.sort(tstk);
+        		min = tstk.get(0);
+        		minI = 1;
+    		}
+    	} else minI--;
+    }
+    
+    public int top() {
+        return ls.get(ls.size() - 1);
+    }
+    
+    public int getMin() {
+    	if (minI > 0) return min;
+    	else {
+    		System.out.print("getmin: ");
+        	List<Integer> tstk = new ArrayList<Integer>();
+        	tstk.addAll(ls);
+        	Collections.sort(tstk);
+        	System.out.println(tstk.get(0));
+        	return tstk.get(0);
+    	}
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+//20160705Tue11:24 duration:38m33s58 Accepted @github.com/BryanBo-Cao,hackerrank.com/bryanbocao,linkedin.com/in/bryanbocao
