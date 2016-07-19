3fbac6985d16cfba1836ff68d44b52203855890e
dingwenxiao <stephen150150@gmail.com>
2016-07-19 14:05:32 -0400

    move my code into dingwen_firstRound folder

--- Checker.java
+++
@@ -1,8 +0,0 @@
-// java Comparator, Hackerrank
-class Checker implements Comparator<Player> {
-    @Override
-    public int compare(Player a, Player b) {
-        return a.score!=b.score? b.score - a.score : a.name.compareTo(b.name);
-    }
-    
-}
