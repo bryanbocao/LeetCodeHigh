524eb235721ca43cad4f2d5730faf9e761d5a114
bryanbo-cao <bryan.bo.cao@outlook.com>
2016-08-03 17:01:58 -0600

    Tried num3379_KthSmallestElementInASortedMatrix Will try another strategy tomorrow

---
+++ BryanBo-Cao/Algorithms/Java/num379_KthSmallestElementInASortedMatrix/Test.java
@@ -0,0 +1,10 @@
+package algorithms.num379_KthSmallestElementInASortedMatrix;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Solution slt = new Solution();
+		System.out.println(slt.kthSmallest());
+	}
+
+}
