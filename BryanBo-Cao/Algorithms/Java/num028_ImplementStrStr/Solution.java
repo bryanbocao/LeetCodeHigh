69b29d4fc75142ce8f93deb49e2cc0b9d3231a75
bryanbo-cao <bryan.bo.cao@outlook.com>
2016-08-04 21:43:15 -0600

    Solved num028_ImplementStrStr duration:4m19s00

---
+++ BryanBo-Cao/Algorithms/Java/num028_ImplementStrStr/Solution.java
@@ -0,0 +1,16 @@
+/**
+https://leetcode.com/problems/implement-strstr/
+28. Implement strStr()  QuestionEditorial Solution  My Submissions
+Total Accepted: 119569 Total Submissions: 465795 Difficulty: Easy
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Subscribe to see which companies asked this question
+ */
+package algorithms.num028_ImplementStrStr;
+
+public class Solution {
+    public int strStr(String haystack, String needle) { return haystack.indexOf(needle); }
+}
+//20160804Thu21:41 duration:4m19s00 Accepted @github.com/BryanBo-Cao,hackerrank.com/bryanbocao,leetcode.com/bryanbocao-0/,linkedin.com/in/bryanbocao 
