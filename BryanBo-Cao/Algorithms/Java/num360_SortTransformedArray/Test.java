ce1620fe1f10c706d09ed73d13318849509f96ce
BryanBo-Cao <bryan.bo.cao@outlook.com>
2016-08-15 21:58:09 -0400

    Solved num360_SortTransformedArray CodingDuration:33m57s37

---
+++ BryanBo-Cao/Algorithms/Java/num360_SortTransformedArray/Test.java
@@ -0,0 +1,11 @@
+package algorithms.num360_SortTransformedArray;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a = 5, b = 3;
+		double midI = -b/(2*(double)a);
+		System.out.println(midI);
+	}
+
+}
