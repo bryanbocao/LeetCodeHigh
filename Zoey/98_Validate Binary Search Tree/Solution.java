6ab3f429f7d8b3bb4b3e31cc324f283a285cb462
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-08-19 01:09:07 -0600

    98

---
+++ Zoey/98_Validate Binary Search Tree/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+
+    public boolean helper(TreeNode root, long min, long max){
+        if(root==null) return true;
+        if(root.val>=max || root.val<=min) return false;
+        return helper(root.left,min,(long)root.val) && helper(root.right,(long)root.val,max);
+    }
+}
