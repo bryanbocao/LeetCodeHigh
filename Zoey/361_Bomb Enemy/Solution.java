cfd4d21da152ffc5ad6f62659d9dcf1f74c22c3b
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-09-18 12:40:00 +0000

    361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361361

---
+++ Zoey/361_Bomb Enemy/Solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int maxKilledEnemies(char[][] grid) {
+        if(grid==null || grid.length==0 || grid[0].length==0) return 0;
+        int max=0, row=0;
+        int[] col = new int[grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='W') continue;
+                if(j==0 || grid[i][j-1]=='W') row=killedERow(grid,i,j);
+                if(i==0 || grid[i-1][j]=='W') col[j]=killedECol(grid,i,j);
+                if(grid[i][j]=='0') max = Math.max(max,row+col[j]);
+            }
+        }
+        return max;
+    }
+
+    private int killedERow(char[][] grid, int i, int j){
+        int num = 0;
+        while(j<=grid[0].length-1 && grid[i][j]!='W'){
+            if(grid[i][j]=='E') num++;
+            j++;
+        }
+        return num;
+    }
+
+    private int killedECol(char[][] grid, int i, int j){
+        int num = 0;
+        while(i<=grid.length-1 && grid[i][j]!='W'){
+            if(grid[i][j]=='E') num++;
+            i++;
+        }
+        return num;
+    }
+}
