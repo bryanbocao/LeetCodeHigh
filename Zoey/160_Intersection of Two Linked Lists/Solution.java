798c38ef0930ff82ecc209afefe4482d064dcec7
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-10-01 16:05:14 -0600

    160

---
+++ Zoey/160_Intersection of Two Linked Lists/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode l1=headA, l2=headB;
+        if(l1==null || l2==null) return null;
+        while(l1!=null && l2!=null && l1!=l2){
+            l1=l1.next;
+            l2=l2.next;
+            if(l1==l2) return l1;
+            if(l1==null){
+                l1=headB;
+            }
+            if(l2==null){
+                l2=headA;
+            }
+        }
+        return l1;
+    }
+}
