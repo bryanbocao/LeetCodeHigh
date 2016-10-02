61b46d7b12f0f6b36ef4c179006053418df93b36
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-10-01 18:38:49 -0600

    102

---
+++ Zoey/102_Binary Tree Level Order Traversal/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        queue.offer(root);
+        if(root==null) return wrapList;
+        while(!queue.isEmpty()){
+            int level = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0;i<level;i++){
+                if(queue.peek().left!=null) queue.offer(queue.peek().left);
+                if(queue.peek().right!=null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+}
