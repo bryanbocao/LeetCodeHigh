0657a02c7c3be9d50e08bc1538992c5ca6cae516
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-10-27 16:22:07 -0600

    101

---
+++ Zoey/101_Symmetric Tree/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//non recursive solution
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        if(root.left!=null){
+            if(root.right==null) return false;
+            stack.push(root.left);
+            stack.push(root.right);
+        }else if(root.right!=null) return false;
+        while(!stack.isEmpty()){
+            TreeNode right = stack.pop();
+            TreeNode left = stack.pop();
+            if(right.val!=left.val) return false;
+            if(left.left!=null){
+                if(right.right==null) return false;
+                stack.push(left.left);
+                stack.push(right.right);
+            }else if(right.right!=null) return false;
+
+            if(left.right!=null){
+                if(right.left==null) return false;
+                stack.push(left.right);
+                stack.push(right.left);
+            }else if(right.left!=null) return false;
+        }
+        return true;
+    }
+}
