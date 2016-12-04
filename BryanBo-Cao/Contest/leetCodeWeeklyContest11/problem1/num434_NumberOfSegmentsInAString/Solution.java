0a034689e78e765ce5ca887841e718c31bd0bbaf
BryanBo-Cao <bryan.bo.cao@outlook.com>
2016-12-03 21:48:34 -0700

    Tried num434_NumberOfSegmentsInAString codingDuration:1m08s88 WrongAnswer

---
+++ BryanBo-Cao/Contest/leetCodeWeeklyContest11/problem1/num434_NumberOfSegmentsInAString/Solution.java
@@ -0,0 +1,36 @@
+package leetCodeWeeklyContest11.problem1.num434_NumberOfSegmentsInAString;
+
+public class Solution {
+    public int countSegments(String s) {
+        return s.split(" ").length;
+    }
+}
+//https://leetcode.com/contest/leetcode-weekly-contest-11/problems/number-of-words-in-a-string/
+/*
+434. Number of Segments in a String My SubmissionsBack To Contest
+User Accepted: 451
+User Tried: 861
+Total Accepted: 484
+Total Submissions: 997
+Difficulty: Easy
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+For example,
+
+Input: "Hello, my name is John"
+
+Output: 5
+ */
+//TriedOn20161203SatNight CodingDuration:1m08s88 WrongAnswer @github.com/BryanBo-Cao,hackerrank.com/bryanbocao,leetcode.com/bryanbocao-0/,linkedin.com/in/bryanbocao
+/*
+Submission Details
+20 / 26 test cases passed.
+Status: Wrong Answer
+Submitted: 1 hour, 55 minutes ago
+Input:
+""
+Output:
+1
+Expected:
+0
+*/
